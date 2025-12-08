f4ca88cbff7bb89da441e92f776ea2ec60d2db3b
Anurag Sharma Roy <anuragsharmaroy@gmail.com>
2025-12-08 13:40:40 +0530

    adding webservice file

---
+++ jswebservice.js
@@ -0,0 +1,9 @@
+var express=require('express')
+var app=express()
+
+app.get('/',function(request,response){
+    response.send('Hello World,'+new Date().toLocaleString())
+
+})
+
+app.listen(8989);
