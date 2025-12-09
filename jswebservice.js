9180ac4d7ce9ef1e547ce857d4d0818409fd5a62
Anurag Sharma Roy <anuragsharmaroy@gmail.com>
2025-12-09 13:02:04 +0530

    adding some more functionalities

--- jswebservice.js
+++ jswebservice.js
@@ -2,8 +2,20 @@ var express=require('express')
 var app=express()
 
 app.get('/',function(request,response){
-    response.send('Hello World,'+new Date().toLocaleString())
+    response.send('Hello World,'+new Date().toLocaleTimeString())
+
+})
+
+app.get('/hello',function(request,response){
+    response.send('Hello World, how are you?')
+
+})
+
+app.get('/emp',function(request,response){
+    data=[{"emp_id":1,"emp_name":"Anurag","emp_salary":1000},{"emp_id":1,"emp_name":"Anurag","emp_salary":1000},{"emp_id":1,"emp_name":"Anurag","emp_salary":1000}]
+    response.send(data)
 
 })
 
 app.listen(8988);
+console.log("Server is running on node 8988")
